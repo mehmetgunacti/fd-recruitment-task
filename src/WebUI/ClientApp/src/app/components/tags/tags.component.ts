f3b969b286c08052a944d89908588e9ca3a80b4b
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-03-12 20:47:09 +0300

    add tags component

---
+++ src/WebUI/ClientApp/src/app/components/tags/tags.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, computed, input, output, signal } from '@angular/core';
+import { TodoListDto } from 'src/app/web-api-client';
+
+function reduceTags(list: TodoListDto): string[] {
+
+  if (!list.items)
+    return [];
+
+  const uniqueTags = new Set<string>(
+    list.items
+      .map(item => item.tagList || [])
+      .reduce((acc, cur) => acc.concat(cur), [])
+  );
+
+  return Array.from(uniqueTags);
+
+}
+
+@Component({
+  selector: 'app-tags',
+  imports: [],
+  templateUrl: './tags.component.html',
+  styleUrl: './tags.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TagsComponent {
+
+  // Input
+  selectedList = input.required<TodoListDto>();
+
+  // Output
+  tagSelected = output<string | null>();
+
+  protected selectedTag = signal<string | null>(null);
+  protected tags = computed(() => reduceTags(this.selectedList()));
+
+  selectTag(tag: string): void {
+
+    const newTag = this.selectedTag() === tag ? null : tag; // de-select on 2nd click
+    this.selectedTag.set(newTag);
+    this.tagSelected.emit(newTag);
+
+  }
+
+}
