1d6d3ff22547140c20c643c97d854dd6938373e6
Mehmet Gunacti <mehmet.gunacti@gmail.com>
2025-03-13 01:59:35 +0300

    migration

---
+++ src/Infrastructure/Persistence/Migrations/20250312225902_AddSoftDeleteFields.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo_App.Infrastructure.Persistence.Migrations
+{
+    public partial class AddSoftDeleteFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "TodoLists",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "TodoLists",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "TodoItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "TodoItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "TodoItems");
+        }
+    }
+}
